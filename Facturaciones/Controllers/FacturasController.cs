f5d71b5554acd15e2ab8f3248c11a540a900a1a9
Alan Masriera PC <alan.masriera@gmail.com>
2022-05-06 12:53:53 -0300

    First commit

---
+++ Facturaciones/Controllers/FacturasController.cs
@@ -0,0 +1,24 @@
+﻿using Facturaciones.DAL;
+using Facturaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Facturaciones.Controllers
+{
+    public class FacturasController : Controller
+    {
+        FacturasRepository repo = new FacturasRepository();
+        // GET: Facturas
+        public ActionResult Index()
+        {
+            IEnumerable<Facturas> lista=  repo.GetFacturasPorPrestador("Alan Masriera");
+            return View(lista);
+        }
+
+        // GET: Facturas/Details/5
+
+    }
+}
